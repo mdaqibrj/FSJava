d8f3954ec8f4334ab3e13ddfe1de7b4f0ff81ba9
Aqib <m.rasheedjavid@lifeworks.com>
2024-03-22 10:30:41 -0400

    Java file - App.java - printing hello world

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+class App
+{
+    public static void main( String[] args )
+    {
+        String name = "Aqib";
+        System.out.println( "Hello World! "+name);
+    }
+}
